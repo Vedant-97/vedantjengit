386200e6b361420fe2e294a86859eeeb8325f499
Vedant-97 <69512114+Vedant-97@users.noreply.github.com>
2022-02-25 12:40:47 +0530

    Update experiment.tf

--- experiment.tf
+++ experiment.tf
@@ -4,7 +4,7 @@ access_key= "AKIAZ3S6MYW2UNTUOT7S"
 secret_key= "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"
 }
 
-resource "aws_instance" "myserver2" {
+resource "aws_instance" "ms123456" {
   ami           = "ami-0a4e637babb7b0a86"
   instance_type = "t2.medium"
   tags = {
